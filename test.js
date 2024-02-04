203e16e531ae5690399607e7ee5e3083cdeba062
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-04 04:24:52 +0200

    :"(

---
+++ test.js
@@ -0,0 +1,8 @@
+let pageTitle = "Elzero", pageDescription = "Elzero web school", pageDate = 25/10
+let hell = `<div>
+<h3>Hello${pageTitle}</h3>
+<p>${pageDescription}</p>
+<span>${pageDate}</span>
+</div>
+`;
+document.write(hell.repeat(4));
